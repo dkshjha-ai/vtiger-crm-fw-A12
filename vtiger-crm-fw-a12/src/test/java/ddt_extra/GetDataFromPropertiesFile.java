4aa1d3ce58d6ecfa35bf4f713524cf67d7072667
Diksha <Diksha@192.168.1.37>
2025-09-29 01:02:05 +0530

    This is the first commit

---
+++ vtiger-crm-fw-a12/src/test/java/ddt_extra/GetDataFromPropertiesFile.java
@@ -0,0 +1,32 @@
+package ddt_extra;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GetDataFromPropertiesFile {
+
+	public static void main(String[] args) throws IOException {
+		//   1. Get The Java Representation Object Of The Physical File
+		FileInputStream fis = new FileInputStream("./src/test/resources/CommonData.properties");
+		
+        // 2. Use the load() of Properties class to load all the keys
+		Properties pObj= new Properties();
+		pObj.load(fis);
+		
+        //3.  Use the getProperty(Key) and pass the key and get the value in string format
+		String BROWSER = pObj.getProperty("bro");
+		String URL = pObj.getProperty("url");
+		String USERNAME = pObj.getProperty("un");
+		String PASSWORD = pObj.getProperty("pwd");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
+		
+		
+	}
+
+}
