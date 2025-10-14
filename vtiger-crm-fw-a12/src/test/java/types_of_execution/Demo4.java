3118a83f734895df41ae05f7b5138e116389815a
Diksha <Diksha@192.168.1.33>
2025-10-14 10:11:41 +0530

    @Parameters

--- vtiger-crm-fw-a12/src/test/java/types_of_execution/Demo4.java
+++ vtiger-crm-fw-a12/src/test/java/types_of_execution/Demo4.java
@@ -1,13 +1,31 @@
 package types_of_execution;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class Demo4{
+public class Demo4 {
+	
+	@Parameters("bro")
 	@Test
-	public void case4() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
+	public void case4(String Browser) throws InterruptedException, IOException {
+		WebDriver driver = null;
+
+//		String Browser = new FileUtility().getDataFromPropertiesFile("bro");
+		
+		if (Browser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}else if (Browser.equals("edge")) {
+			driver = new EdgeDriver();
+		}else if (Browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		
 		Thread.sleep(5000);
 		driver.quit();
 	}
--- vtiger-crm-fw-a12/src/test/java/types_of_execution/Demo5.java
+++ vtiger-crm-fw-a12/src/test/java/types_of_execution/Demo5.java
@@ -2,12 +2,27 @@ package types_of_execution;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Demo5{
+	@Parameters("bro")
 	@Test
-	public void case5() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
+	public void case5(String Browser) throws InterruptedException {
+		WebDriver driver = null;
+		
+//		String Browser = new FileUtility().getDataFromPropertiesFile("bro");
+		
+		if(Browser.equals("chrome")) {
+			driver = new ChromeDriver();
+		} else if(Browser.equals("edge")) {
+			driver= new EdgeDriver();
+		} else if(Browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		
 		Thread.sleep(5000);
 		driver.quit();
 	}
